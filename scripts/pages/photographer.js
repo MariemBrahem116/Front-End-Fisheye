9880123ccafa3fb902fc0e5e46539114c9fde442
MariemBrahem116 <112781653+MariemBrahem116@users.noreply.github.com>
2022-12-23 15:38:42 +0100

    nouvelle modification

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,18 @@
 //Mettre le code JavaScript lié à la page photographer.html
+let params = (new URL(document.location)).searchParams; 
+const linkData = "./../data/photographers.json"
+        fetch(linkData)
+          .then((response) => response.json())
+            
+          .then((data) => displayPhotographerID(data.photographers,params))
+         
+          function displayPhotographerID(photographers,params) {
+            
+                if(photographers.id===params.get('id')){
+        
+            photographerFactoryID(photographers);
+            
+            }     
+         }
+       
+
