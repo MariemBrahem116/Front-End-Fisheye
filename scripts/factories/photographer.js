0c5d95f60680f0138bbdbcde7faa52e1889a7444
MariemBrahem116 <112781653+MariemBrahem116@users.noreply.github.com>
2022-12-23 16:12:54 +0100

    nouvelle modification

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,76 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+function photographerFactory(photographer) {
+    const linkToPage = "photographer.html?id=" + photographer.id;
+    const linkToPhoto =
+        "./assets/photographers/" + photographer.portrait;
+    const cardPhotographer = document.querySelector(".photographer_section");
+    const cardPhtographers = document.createElement("div")
+    const cardLink = document.createElement("a");
+    const cardImg = document.createElement("img");
+    const cardTitle = document.createElement("h2");
+    const cardBody = document.createElement("div");
+    const cardLocation = document.createElement("h3");
+    const cardTagline = document.createElement("p");
+    const cardPrice = document.createElement("p");
+    cardPhtographers.classList.add("photographer_cards");
+    cardImg.classList.add("cards-img");
+    cardTitle.classList.add("cards-title");
+    cardLocation.classList.add("cards-location");
+    cardLink.classList.add("cards-photographer-link");
+    cardTagline.classList.add("cards-tagline");
+    cardPrice.classList.add("cards-price");
+    cardBody.classList.add("cards-body");
+    cardLink.setAttribute("role", "link");
+    cardLink.href = linkToPage;
+    cardImg.setAttribute("src",linkToPhoto);
+  //  cardImg.alt = "";
+    cardTitle.textContent = photographer.name;
+    cardLocation.textContent = photographer.city + ", " + photographer.country;
+    cardTagline.textContent = photographer.tagline;
+    cardPrice.textContent = photographer.price + "€/jour";
+    
+    cardBody.appendChild(cardLocation);
+    cardBody.appendChild(cardTagline);
+    cardBody.appendChild(cardPrice);
+    cardLink.appendChild(cardImg);
+    cardLink.appendChild(cardTitle);
+    cardPhtographers.appendChild(cardLink);
+    cardPhtographers.appendChild(cardBody);
+    cardPhotographer.appendChild(cardPhtographers)
+    main.appendChild(cardPhotographer);
 }
+function photographerFactoryID(currentPhotographer) {
+    const main = document.querySelector("#main");
+        const linkToPhoto =
+    "./assets/photographers/" + currentPhotographer.portrait;
+    const photographHeader = document.querySelector(".photograph-header");
+    const btnModal = document.querySelector(".contact_button");
+    //  création des elements html
+    const photographerBody = document.createElement("div");
+    const photographerTitle = document.createElement("h1");
+    const photographerLocation = document.createElement("p");
+    const photographerTagline = document.createElement("p");
+    const photographerImg = document.createElement("img");
+    // ajouts des classes et attributs html
+  
+    photographerBody.classList.add("photographer-body");
+    photographerImg.classList.add("photographer-img");
+    photographerTitle.classList.add("photographer-body-title");
+    photographerLocation.classList.add("photographer-body-location");
+    photographerTagline.classList.add("photographer-body-tagline");
+    photographerImg.src = linkToPhoto;
+    photographerImg.setAttribute("alt", `${currentPhotographer.name}`);
+  
+    photographerTitle.textContent = currentPhotographer.name;
+    photographerLocation.textContent =
+      currentPhotographer.city + " ," + currentPhotographer.country;
+      photographerTagline.textContent = currentPhotographer.tagline;  
+  
+    // ajout des elements dans le DOM
+    photographerBody.appendChild(photographerTitle);
+    photographerBody.appendChild(photographerLocation);
+    photographerBody.appendChild(photographerTagline);
+    photographHeader.appendChild(photographerBody);
+    photographHeader.appendChild(btnModal);
+    main.appendChild(photographHeader);
+  }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,116 @@
+const photgraphHeader = document.querySelector(".photograph-header");
+const logoHeader = document.querySelector(".logo-Header");
+const modal = document.getElementById("contact_modal");
+const confirmMessage = document.querySelector(".confirm-message");
+const containerInput = document.querySelectorAll(".formData");
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    photgraphHeader.style.display = "none";
+    logoHeader.style.display = "none";
+    confirmMessage.style.display = "none";
 }
 
-function closeModal() {
-    const modal = document.getElementById("contact_modal");
+function closeModal() { 
     modal.style.display = "none";
+    photgraphHeader.style.display = "block";
+    logoHeader.style.display = "block";
 }
+//Accéder à toutes les entrées et paramétrer la validation
+//Indiquer si l'expression régulière trouve une correspondance dans la chaîne d'entrée
+const inputs = {
+    firstname: {
+        element: document.querySelector("#prenom"),
+        regex : /^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ]{2,}$/
+    }, 
+    lastname: {
+        element: document.querySelector("#nom"),
+        regex : /^[a-zA-Z\-éëàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇÆæœ\s]{2,}$/,
+    },
+    email: {
+        element: document.querySelector("#email"),
+        regex : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    },
+  
+    message: {
+        element: document.querySelector("#msg"), 
+    }
+  };
+  // pour atteindre tous les éléments qui affichent des messages d'erreur pour la validation du formulaire
+   class validate {  
+    constructor(inputs, containerInput) {
+        this.inputs = inputs;
+        this.containerInput = containerInput;
+    }
+    valid = true;
+    // Les méthodes de validation
+        //vérifier si le nom et le prénom ne sont pas vide et ils comportent  au minimum 2 lettres 
+        nameValidation = (element, regex) => element.value == "" || element.value.length < 2 || !regex.test(element.value) ? false : true;
+  
+        // vérifier si le mail est valide
+        emailValidation = (element, regex) => !regex.test(element.value) ? false : true;
+  
+  
+        // vérifier si la case message est vide
+        messageValidation = (element) => element.value == "";
+  
+    //supprimer les messages d'erreur
+    removeErrorMessage = () => {
+        this.valid = true;
+        Array.from(this.containerInput).map((field) => {
+            field.removeAttribute("data-error-visible");
+        });
+    };
+  
+    launchValidation = () => {
+        this.removeErrorMessage();
+        for (const input in this.inputs) {
+            switch (input) {
+                case "firstname":
+                    if (!this.nameValidation(this.inputs[input].element, this.inputs[input].regex)) {
+                        console.log(this.inputs[input].regex)
+                        this.inputs[input].element.parentNode.setAttribute("data-error-visible", true);
+                        this.valid = false;
+                    }
+                    break;
+  
+                case "lastname":
+                    if (!this.nameValidation(this.inputs[input].element, this.inputs[input].regex)) {
+                        this.inputs[input].element.parentNode.setAttribute("data-error-visible", true);
+                        this.valid = false;
+                    }
+                    break;
+  
+                case "email":
+                    if (!this.emailValidation(this.inputs[input].element, this.inputs[input].regex)) {
+                        this.inputs[input].element.parentNode.setAttribute("data-error-visible", true);
+                        this.valid = false;
+                    }
+                    break;
+  
+                case "message":
+                    if (!this.messageValidation(this.inputs[input].element)) {
+                        this.inputs[input].element.parentNode.setAttribute("data-error-visible", true);
+                        this.valid = false;
+                    }
+                    break;
+  
+                default : 
+                    break;
+            }
+        }
+        return this.valid;
+    }
+  }
+  // Valider le formulaire en cas de remplissage de donnée sans erreur
+  const validator = new validate(inputs, containerInput);
+  modal.onsubmit = (e) => {
+      e.preventDefault();
+      if (validator.launchValidation()) {
+          modal.reset();
+          modal.style.display ="none";
+          confirmMessage.style.display ="flex";
+      }
+  }
+  
+  
+  
